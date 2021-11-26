1ece580a31ffbd9c2b92901ceb2eda962f885c43
Somesh Arora <somesh.arora@microsoft.com>
2021-11-26 14:33:02 -0800

    Initial Commit

---
+++ Stocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stocks
+//
+//  Created by Somesh Arora on 11/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Somesh Arora on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
